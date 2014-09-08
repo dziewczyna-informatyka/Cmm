20c188b8c22b0ac772fbe81c3443ad5b02a42b81
Pawel Burzynski <p.k.burzynski@gmail.com>
2014-09-08 22:12:52 +0100

    MainContext added to base controller

--- src/MaintenanceManagement.Web/Core/BaseController.cs
+++ src/MaintenanceManagement.Web/Core/BaseController.cs
@@ -2,9 +2,27 @@
 {
     using System.Web.Mvc;
 
+    using MaintenanceManagement.DataAccess;
+
     [Authorize]
     public class BaseController : Controller
     {
-         
+        public BaseController()
+        {
+            MainContext = new MainContext();
+        }
+
+        protected MainContext MainContext { get; set; }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (disposing)
+            {
+                MainContext.Dispose();
+                MainContext = null;
+            }
+        }
     }
 }
