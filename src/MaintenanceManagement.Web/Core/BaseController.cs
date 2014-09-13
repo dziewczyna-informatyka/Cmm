2c031aa5383b5c6e9c74f723217b79535c454127
Pawel Burzynski <p.k.burzynski@gmail.com>
2014-09-13 13:35:22 +0100

    culture change stub

--- src/MaintenanceManagement.Web/Core/BaseController.cs
+++ src/MaintenanceManagement.Web/Core/BaseController.cs
@@ -1,5 +1,7 @@
 ﻿namespace MaintenanceManagement.Web.Core
 {
+    using System;
+    using System.Threading;
     using System.Web.Mvc;
 
     using MaintenanceManagement.DataAccess;
@@ -13,6 +15,14 @@
         }
 
         protected MainContext MainContext { get; set; }
+
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pl-PL");
+            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+
+            return base.BeginExecuteCore(callback, state);
+        }
 
         protected override void Dispose(bool disposing)
         {
