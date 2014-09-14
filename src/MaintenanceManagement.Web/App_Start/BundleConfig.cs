aaa970c82e44b7f4930b0ca08af4462462aabcd5
Pawel Burzynski <p.k.burzynski@gmail.com>
2014-09-14 14:43:51 +0100

    jQuery UI added

--- src/MaintenanceManagement.Web/App_Start/BundleConfig.cs
+++ src/MaintenanceManagement.Web/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -21,6 +22,7 @@
                       "~/Scripts/bootstrap-datepicker.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/themes/base/*.css",
                       "~/Content/datepicker.css",
                       "~/Content/font-awesome.css"));
 
